91d0ea8bcf754abff046d147646c771d543e12eb
Akhil Nagpal <akhilnagpal07@gmail.com>
2025-09-15 04:19:47 +0530

    feat: add chart for SWP calculation projection

--- src/components/swp-calc.tsx
+++ src/components/swp-calc.tsx
@@ -2,14 +2,22 @@
 
 import { useState } from "react";
 import InputSlider from "./inputSlider";
-import { IconCurrencyRupee } from "@tabler/icons-react";
+import { IconCurrencyRupee, IconRefresh } from "@tabler/icons-react";
 import { formatINR } from "@/lib/currencyFormatter";
+import { ChartAreaGradient } from "./ui/area-chart-gradient";
 
 export default function SWPCalc() {
   const [totalInvestment, setTotalInvestment] = useState<number>(1000000);
   const [withdrawlAmt, setWithdawlAmt] = useState<number>(10000);
   const [expectedRoi, setExpectedRoi] = useState<number>(15);
   const [tenure, setTenure] = useState<number>(10);
+
+  const handleReset = () => {
+    setTotalInvestment(1000000);
+    setWithdawlAmt(10000);
+    setExpectedRoi(15);
+    setTenure(10);
+  };
 
   const calculateSWP = () => {
     const P = totalInvestment;
@@ -31,6 +39,35 @@ export default function SWPCalc() {
   };
 
   const { totalInvested, withdrawls, totalAmount } = calculateSWP();
+
+  const generateChartData = () => {
+    const P = totalInvestment;
+    const w = withdrawlAmt;
+    const r = expectedRoi / 12 / 100;
+
+    const data = [];
+
+    for (let year = 1; year <= tenure; year++) {
+      const months = year * 12;
+
+      const finalValue =
+        P * Math.pow(1 + r, months) - w * ((Math.pow(1 + r, months) - 1) / r);
+
+      const withdrawlValue = w * months;
+      const totalValue = finalValue;
+      const investedAmount = P;
+
+      data.push({
+        year: `Yr ${year}`,
+        invested: Math.floor(investedAmount),
+        returns: Math.floor(withdrawlValue),
+        total: Math.floor(totalValue),
+      });
+    }
+    return data;
+  };
+
+  const chartData = generateChartData();
 
   return (
     <>
@@ -81,6 +118,17 @@ export default function SWPCalc() {
             value={tenure}
             onChange={setTenure}
           />
+          <div className="flex justify-end">
+            <button
+              onClick={handleReset}
+              className="btn-secondary flex gap-2 items-center cursor-pointer text-right"
+            >
+              Reset{" "}
+              <span>
+                <IconRefresh size={22} />
+              </span>
+            </button>
+          </div>
         </div>
         {/* Chart */}
         <div className="bg-white rounded-xl shadow-xs space-y-3 py-10 px-5">
@@ -125,6 +173,11 @@ export default function SWPCalc() {
               </div>
             </div>
           </div>
+          <ChartAreaGradient
+            data={chartData}
+            title="SWP Investment Growth"
+            description="Track withdrawals and remaining value over time"
+          />
         </div>
       </section>
     </>
